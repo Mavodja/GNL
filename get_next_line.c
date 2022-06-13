93c35d73a51607af17c5afe860c6b6d1689fff14
Anthony Costa <acosta@k2r1p5.42mulhouse.fr>
2022-06-13 18:33:04 +0200

    second commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: acosta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 15:23:56 by acosta            #+#    #+#             */
-/*   Updated: 2022/05/25 11:39:16 by acosta           ###   ########.fr       */
+/*   Updated: 2022/06/13 18:11:59 by acosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,32 @@
 
 char	*get_next_line(int fd)
 {
-
+	char	ligne[4];
+	
+	read(fd, ligne, BUFFER_SIZE);
+//premiere partie qui renvoie une ligne jusqu'au retour
+	//lire la ligne jusqu'a la fin
+	//l'enregistrer, la retourner
+	return(&ligne);
 }
+
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main()
+{
+	int		fd;
+	char	*str;
+
+	fd = open("panama.papers", O_RDONLY);
+	str = get_next_line(fd);
+	printf("%s", str);
+//	free(str);
+//	str = get_next_line(fd);
+//	printf("%s", str);
+	close(fd);
+	return(0);
+}
+
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: acosta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 15:26:39 by acosta            #+#    #+#             */
-/*   Updated: 2022/05/18 15:37:59 by acosta           ###   ########.fr       */
+/*   Updated: 2022/06/13 16:57:13 by acosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 35
+# endif
+
 char	*get_next_line(int fd);
+
 #endif
